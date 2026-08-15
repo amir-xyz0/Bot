08484fd2d2630503da0a3d9d2ea8166b5d5e74d8
amir-xyz0 <amirdeveloper33@gmail.com>
2026-08-15 15:11:30 +0330

    Update config.py

--- app/config.py
+++ app/config.py
@@ -11,12 +11,7 @@ class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///bot_data.db")
     
     RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-    if not RAPIDAPI_KEY:
-        raise ValueError("RAPIDAPI_KEY is required!")
     RAPIDAPI_HOST = os.getenv("RAPIDAPI_HOST", "chatgpt-42.p.rapidapi.com")
     CHAT_API_URL = f"https://{RAPIDAPI_HOST}/conversationgpt4-2"
-    
-    MORNING_TIME = os.getenv("MORNING_MESSAGE_TIME", "07:00")
-    NIGHT_TIME = os.getenv("NIGHT_MESSAGE_TIME", "23:00")
 
 config = Config()
