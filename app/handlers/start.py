be9d052a1810334eac70fa54c6c9c7024e8996d0
amir-xyz0 <amirdeveloper33@gmail.com>
2026-08-15 11:52:28 +0330

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -10,17 +10,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = db.query(User).filter_by(user_id=user_id).first()
     
     if user:
-        # کاربر قدیمی → مستقیم به منوی اصلی
         await main_menu(update, context)
     else:
-        # کاربر جدید → ثبت‌نام
-        keyboard = [
-            [InlineKeyboardButton("🚀 شروع ثبت‌نام", callback_data="start_profile")]
-        ]
+        keyboard = [[InlineKeyboardButton("شروع ثبت‌نام", callback_data="start_profile")]]
         await update.message.reply_text(
-            "🤖 **به دستیار همراهت خوش اومدی!**\n\n"
-            "من اینجام تا روزت رو بهتر کنم.\n"
-            "بیا پروفایل رو کامل کنیم تا بهتر بشناسمت.",
+            "به دستیار همراه خود خوش آمدید.\nلطفاً برای ادامه، ثبت‌نام را تکمیل کنید.",
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
     db.close()
