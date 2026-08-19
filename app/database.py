44c34f89e3016c4c5ee15de7f6986a523e8641b7
amir-xyz0 <amirdeveloper33@gmail.com>
2026-08-19 16:07:32 +0330

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,13 +5,10 @@ from app.config import config
 
 engine = create_engine(
     config.DATABASE_URL,
-    pool_pre_ping=True,          # بررسی سلامت اتصال قبل از استفاده
-    pool_recycle=3600,           # بازیابی اتصالات بعد از ۱ ساعت
-    pool_size=5,                 # حداکثر ۵ اتصال همزمان
-    max_overflow=10,             # حداکثر ۱۰ اتصال اضافی
-    echo_pool=False,             # لاگ‌های پول رو خاموش کن (برای کاهش خطاها)
-    pool_reset_on_return="commit" # ریست اتصال فقط در زمان commit
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+    pool_recycle=3600
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
