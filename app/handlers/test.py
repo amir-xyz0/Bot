27470c812ef29cfeac4754aa8f406041d6507c2f
amir-xyz0 <amirdeveloper33@gmail.com>
2026-08-23 20:41:49 +0330

    Delete app/handlers/test.py

--- app/handlers/test.py
+++
@@ -1,9 +0,0 @@
-import logging
-from telegram import Update
-from telegram.ext import ContextTypes
-
-logger = logging.getLogger(__name__)
-
-async def test_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info("🔥🔥🔥 test_handler اجرا شد! پیام: " + update.message.text)
-    await update.message.reply_text("✅ پیام دریافت شد!")
