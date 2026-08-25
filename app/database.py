10853b1e969f7c67b296bcac22899c689e64fee3
amir-xyz0 <amirdeveloper33@gmail.com>
2026-08-25 22:16:58 +0330

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,8 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import config
 
-# هیچ connect_args اضافی برای PostgreSQL
-engine = create_engine(config.DATABASE_URL)
+# برای SQLite
+connect_args = {}
+if config.DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(config.DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
