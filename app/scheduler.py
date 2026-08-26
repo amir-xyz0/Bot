03ddd5a93afc0ef4ee7b4b451d1adbd952a5b19a
amir-xyz0 <amirdeveloper33@gmail.com>
2026-08-26 23:41:44 +0330

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -153,7 +153,7 @@ def start_scheduler(app):
     
     scheduler.add_job(
         send_night_messages,
-        CronTrigger(hour=23, minute=39),
+        CronTrigger(hour=23, minute=43),
         id="night_job",
         replace_existing=True
     )
